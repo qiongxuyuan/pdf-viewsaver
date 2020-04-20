c56713c2618fc8627712b70e7d5f70bcb5abec6a
QXY <qiongxuyuan@gmail.com>
2020-04-19 21:44:16 -0700

    upload file to server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
+import {InputGroup, Button, FormControl, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FormData from 'form-data';
+
+const axios = require('axios');
 
 function App() {
+  const handleChange = (event) => {
+    console.log('handle change', event.target.files);
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    const url = 'http://localhost:55000/fileupload';
+    reader.readAsBinaryString(file);
+    reader.onload = function (evt){
+      console.log(evt.target.result);
+      const data = evt.target.result;
+      const formData = new FormData();
+      formData.append('pdf', file);
+      axios.post(
+        url,
+       formData,
+        {
+          headers:{
+            //'Content-Type': 'application/json'
+            'Content-Type': 'multipart/form-data',
+          }
+        }
+        ).then(function(response) {
+          console.log(response.data);
+        }).catch(function (err){
+          console.log(err);
+        });
+    };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form className="col-6">
+          <Form.File 
+            id="inputFile"
+            label="Choose File"
+            name='pdf'
+            custom
+            onChange={handleChange}
+          ></Form.File>
+          <Button>Submit</Button>
+      </Form>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const dot = require('dotenv');
-dot.config()
-const port = process.env.PORT
+dot.config();
+//const fileUpload = require('express-fileupload');
+const multer = require('multer');
+const multerUpload = multer({dest: 'frontend/public/uploads/'});
 
-const {getTestDB} = require('./mongodbOps')
+const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const {getTestDB} = require('./mongodbOps');
+
+//app.use(express.urlencoded());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/testdb', getTestDB);
+
+app.use(cors());
+//app.use(fileUpload());
+app.post('/fileupload', multerUpload.fields([{name: 'pdf', maxCount: 1}]),  (req, res) => {
+    //console.log(req.files);
+    console.log(req.files);
+    
+    res.send(
+        {message: 'file uploaded'}
+    );
+});
+
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
